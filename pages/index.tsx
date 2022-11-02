6d41a066e599636ee3109309ef80e9959c7a3523
Bernardo Mesko <bemesko@gmail.com>
2022-11-01 21:32:26 -0300

    Add request to backend

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,8 @@
 // JSX - Javascript + XML (HTML)
 export default function Home() {
+  fetch("http://localhost:3333/pools/count")
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+
   return <h1>Hello World</h1>;
 }
