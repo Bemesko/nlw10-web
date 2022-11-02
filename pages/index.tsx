13bb4e3e517174c22eee4ae776ccc7d75c578251
Bernardo Mesko <bemesko@gmail.com>
2022-11-01 21:41:24 -0300

    Add pool count to server side props

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,17 @@
 // JSX - Javascript + XML (HTML)
-export default function Home() {
-  return <h1>Hello World</h1>;
+export default function Home(props) {
+  return <h1>Count: {props.count}</h1>;
 }
 
 export const getServerSideProps = async () => {
-  const poolCountRaw = await fetch("http://localhost:3333/pools/count");
-  const poolCount = await poolCountRaw.json();
+  const responseRaw = await fetch("http://localhost:3333/pools/count");
+  const responseJSON = await responseRaw.json();
 
-  console.log(poolCount);
+  console.log(responseJSON);
 
   return {
-    props: {},
+    props: {
+      count: responseJSON.poolCount,
+    },
   };
 };
